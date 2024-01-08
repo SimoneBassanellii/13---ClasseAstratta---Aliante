388024d5f8592a6e23c89eaf61a6b1ebc310dba0
Simone Bassanelli <bassanelli.simone.studente@itispaleocapa.it>
2024-01-08 22:14:57 +0100

    Non riesco a fare funzione per il prezzo

--- 13 - ClasseAstratta - Aliante/Form1.Designer.cs
+++ 13 - ClasseAstratta - Aliante/Form1.Designer.cs
@@ -28,13 +28,362 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.FusoliaraRadio = new System.Windows.Forms.RadioButton();
+            this.AlaRadio = new System.Windows.Forms.RadioButton();
+            this.CodaRadio = new System.Windows.Forms.RadioButton();
+            this.RuotaRadio = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.Aggiungibutton = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Visualizzabutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // FusoliaraRadio
+            // 
+            this.FusoliaraRadio.AutoSize = true;
+            this.FusoliaraRadio.Location = new System.Drawing.Point(48, 37);
+            this.FusoliaraRadio.Name = "FusoliaraRadio";
+            this.FusoliaraRadio.Size = new System.Drawing.Size(67, 17);
+            this.FusoliaraRadio.TabIndex = 0;
+            this.FusoliaraRadio.TabStop = true;
+            this.FusoliaraRadio.Text = "Fusoliera";
+            this.FusoliaraRadio.UseVisualStyleBackColor = true;
+            // 
+            // AlaRadio
+            // 
+            this.AlaRadio.AutoSize = true;
+            this.AlaRadio.Location = new System.Drawing.Point(48, 83);
+            this.AlaRadio.Name = "AlaRadio";
+            this.AlaRadio.Size = new System.Drawing.Size(40, 17);
+            this.AlaRadio.TabIndex = 1;
+            this.AlaRadio.TabStop = true;
+            this.AlaRadio.Text = "Ala";
+            this.AlaRadio.UseVisualStyleBackColor = true;
+            // 
+            // CodaRadio
+            // 
+            this.CodaRadio.AutoSize = true;
+            this.CodaRadio.Location = new System.Drawing.Point(48, 129);
+            this.CodaRadio.Name = "CodaRadio";
+            this.CodaRadio.Size = new System.Drawing.Size(50, 17);
+            this.CodaRadio.TabIndex = 3;
+            this.CodaRadio.TabStop = true;
+            this.CodaRadio.Text = "Coda";
+            this.CodaRadio.UseVisualStyleBackColor = true;
+            // 
+            // RuotaRadio
+            // 
+            this.RuotaRadio.AutoSize = true;
+            this.RuotaRadio.Location = new System.Drawing.Point(48, 205);
+            this.RuotaRadio.Name = "RuotaRadio";
+            this.RuotaRadio.Size = new System.Drawing.Size(54, 17);
+            this.RuotaRadio.TabIndex = 2;
+            this.RuotaRadio.TabStop = true;
+            this.RuotaRadio.Text = "Ruota";
+            this.RuotaRadio.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(140, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(255, 34);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(137, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Lunghezza";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(252, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Larghezza";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(252, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Apertura";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(137, 67);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Lunghezza";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(255, 83);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(140, 83);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(137, 113);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Lunghezza";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(140, 129);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(252, 186);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Larghezza";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(137, 186);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Altezza";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(255, 202);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 15;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(140, 202);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 14;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(358, 189);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(41, 13);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "Raggio";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(361, 205);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 20);
+            this.textBox8.TabIndex = 18;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(137, 163);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(43, 13);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "Gomma";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(137, 235);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(55, 13);
+            this.label10.TabIndex = 25;
+            this.label10.Text = "Cerchione";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(252, 259);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(50, 13);
+            this.label11.TabIndex = 24;
+            this.label11.Text = "Materiale";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(137, 259);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(34, 13);
+            this.label12.TabIndex = 23;
+            this.label12.Text = "Pollici";
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(255, 275);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(100, 20);
+            this.textBox9.TabIndex = 22;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(140, 275);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(100, 20);
+            this.textBox10.TabIndex = 21;
+            // 
+            // Aggiungibutton
+            // 
+            this.Aggiungibutton.ImageAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.Aggiungibutton.Location = new System.Drawing.Point(140, 321);
+            this.Aggiungibutton.Name = "Aggiungibutton";
+            this.Aggiungibutton.Size = new System.Drawing.Size(100, 34);
+            this.Aggiungibutton.TabIndex = 26;
+            this.Aggiungibutton.Text = "Aggiungi";
+            this.Aggiungibutton.UseVisualStyleBackColor = true;
+            this.Aggiungibutton.Click += new System.EventHandler(this.Aggiungibutton_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(492, 18);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(284, 337);
+            this.listView1.TabIndex = 27;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // Visualizzabutton
+            // 
+            this.Visualizzabutton.ImageAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.Visualizzabutton.Location = new System.Drawing.Point(255, 321);
+            this.Visualizzabutton.Name = "Visualizzabutton";
+            this.Visualizzabutton.Size = new System.Drawing.Size(100, 34);
+            this.Visualizzabutton.TabIndex = 28;
+            this.Visualizzabutton.Text = "Visualizza";
+            this.Visualizzabutton.UseVisualStyleBackColor = true;
+            this.Visualizzabutton.Click += new System.EventHandler(this.Visualizzabutton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Visualizzabutton);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.Aggiungibutton);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.CodaRadio);
+            this.Controls.Add(this.RuotaRadio);
+            this.Controls.Add(this.AlaRadio);
+            this.Controls.Add(this.FusoliaraRadio);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RadioButton FusoliaraRadio;
+        private System.Windows.Forms.RadioButton AlaRadio;
+        private System.Windows.Forms.RadioButton CodaRadio;
+        private System.Windows.Forms.RadioButton RuotaRadio;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.Button Aggiungibutton;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button Visualizzabutton;
     }
 }
 
--- 13 - ClasseAstratta - Aliante/Form1.cs
+++ 13 - ClasseAstratta - Aliante/Form1.cs
@@ -7,14 +7,57 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
 
 namespace _13___ClasseAstratta___Aliante
 {
     public partial class Form1 : Form
     {
+        private List<string> values = new List<string>();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Aggiungibutton_Click(object sender, EventArgs e)
+        {
+
+
+            if (FusoliaraRadio.Checked)
+            {
+                values.Add(textBox1.Text);
+                values.Add(textBox2.Text);
+
+            }
+            if (AlaRadio.Checked)
+            {
+                values.Add(textBox4.Text);
+                values.Add(textBox3.Text);
+
+            }
+            if (CodaRadio.Checked)
+            {
+                values.Add(textBox5.Text);
+            }
+            if (RuotaRadio.Checked)
+            {
+                values.Add(textBox7.Text);
+                values.Add(textBox6.Text);
+                values.Add(textBox8.Text);
+                
+                values.Add(textBox10.Text);
+                values.Add(textBox9.Text);
+
+            }
+        }
+
+        private void Visualizzabutton_Click(object sender, EventArgs e)
+        {
+            foreach (string value in values)
+            {
+                listView1.Items.Add(value);
+            }
         }
     }
 }
